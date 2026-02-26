4ff8cbfb89b6b48236b5509a5d48077697269ad2
ArminAustin200 <145053354+ArminAustin200@users.noreply.github.com>
2026-02-26 11:54:02 -0500

    Added product page

---
+++ assets/shop.js
@@ -0,0 +1,14 @@
+// assets/products.js
+window.products = [ {
+  id : "XXXX0000",
+  title : "Coming Soon...",
+  price : 0.00,
+  currency : "USD",
+  soldOut : true,
+  images : ["assets/logo/website_logo.png"],
+  image : "assets/logo/website_logo.png",
+  features : [],
+  stockText : "*SOLD OUT*",
+  description : "",
+  dateAdded : "2026-02-26"
+}];
