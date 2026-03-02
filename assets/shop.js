af7da04e21ad96c66afcae09421dfead965b610f
ArminAustin200 <145053354+ArminAustin200@users.noreply.github.com>
2026-03-02 13:03:32 -0500

    Update shop.js

--- assets/shop.js
+++ assets/shop.js
@@ -8,7 +8,7 @@ window.products = [ {
   images : [ "assets/Shop/FreeRunner/1.jpg" ],
   image : "assets/Shop/FreeRunner/1.jpg",
   features : [ "RGH1.2 support for Phat and Slim", "Multi-NAND support", "XSB support via muxing", "Built-in 68nF cap", "Multiple Debug LEDs for easy debugging" ],
-  stockText : "*Coming Soon...*",
-  description : "ONCE SOLDER HAS TOUCHED THE PRODUCT YOU ARE NO LONGER UNDER ANY WARRANTY.\nMore info on: https://github.com/ArminAustin200/FreeRunner",
+  stockText : "*25 IN STOCK*",
+  description : "ONCE SOLDER HAS TOUCHED THE PRODUCT YOU ARE NO LONGER UNDER ANY WARRANTY. LIMIT 5 PER CUSTOMER.\nMore info on: https://github.com/ArminAustin200/FreeRunner",
   dateAdded : "2026-02-26"
 } ];
