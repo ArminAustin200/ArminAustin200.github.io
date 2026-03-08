200bfbe37c6530e9474109f770df4ad4f1041c11
ArminAustin200 <145053354+ArminAustin200@users.noreply.github.com>
2026-03-08 00:32:23 -0500

    Update shop.js

--- assets/shop.js
+++ assets/shop.js
@@ -4,11 +4,11 @@ window.products = [ {
   title : "FreeRunner LIMITED EDITION (Launch Edition)",
   price : 8.8,
   currency : "USD",
-  soldOut : false,
+  soldOut : true,
   images : [ "assets/Shop/FreeRunner/1.jpg" ],
   image : "assets/Shop/FreeRunner/1.jpg",
   features : [ "RGH1.2 support for Phat and Slim", "Multi-NAND support", "XSB support via muxing", "Built-in 68nF cap", "Multiple Debug LEDs for easy debugging" ],
-  stockText : "*ONLY 1 LEFT IN STOCK!!!*",
+  stockText : "*SOLD OUT*",
   description : "ONCE SOLDER HAS TOUCHED THE PRODUCT YOU ARE NO LONGER UNDER ANY WARRANTY. LIMIT 1 PER CUSTOMER.\nMore info on: https://github.com/ArminAustin200/FreeRunner",
   dateAdded : "2026-02-26"
 } ];
